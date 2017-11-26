e854608269043176b452331b26e3bb9ad24988d0
Zhanrui Liang <ray040123@gmail.com>
2017-11-26 01:18:57 -0800

    Refactor visual options

--- src/sheet2tab.js
+++ src/sheet2tab.js
@@ -2,21 +2,41 @@
 let app = angular.module('sheet2tab', []);
 let VF = Vex.Flow;
 
-class MainCtrl {
-  constructor() {
-    this.music = "mmmmm";
+const defaultVisualOptions = {
+  width: 800,
+  height: 1200,
+  marginLeft: 20,
+  marginTop: 50,
+  marginRight: 20,
+  marginBottom: 80,
+}
+
+class VisualOptions {
+  constructor(options) {
+    options = options || {}
+    this.width = options.width || defaultVisualOptions.width;
+    this.height = options.height || defaultVisualOptions.height;
+    this.marginLeft = options.marginLeft || defaultVisualOptions.marginLeft;
+    this.marginRight = options.marginRight || defaultVisualOptions.marginRight;
+    this.marginTop = options.marginTop || defaultVisualOptions.marginTop;
+    this.marginBottom = options.marginBottom || defaultVisualOptions.marginBottom;
   }
 }
 
-function render(elem, music, width, height) {
+class MainCtrl {
+  constructor() {
+    this.music = "mmmmm";
+    this.visopts = new VisualOptions();
+  }
+}
+
+function render(elem, visopts, music) {
   let rawElem = elem[0];
-  width = parseInt(width);
-  height = parseInt(height);
   let renderer = new VF.Renderer(rawElem, VF.Renderer.Backends.SVG);
-  renderer.resize(width, height);
+  renderer.resize(visopts.width, visopts.height);
   let ctx = renderer.getContext();
   ctx.setFont("Arial", 10, "");
-  let stave = new VF.Stave(10, 10, 400);
+  let stave = new VF.Stave(visopts.marginLeft, visopts.marginTop, 400);
   stave.addClef("treble").addTimeSignature("4/4");
   stave.setContext(ctx).draw();
 }
@@ -28,15 +48,14 @@ app.directive("vfCanvas", function() {
     replace: true,
     template: '<div class="vf-canvas" id="{{id}}"></div>',
     scope: {
-      width: "@",
-      height: "@",
       id: "@",
       music: "=",
+      visopts: "=",
     },
     link: function(scope, elem, attrs) {
-      scope.$watch('music', function() {
-        render(elem, scope.music, scope.width, scope.height);
-      });
+      let refresh = () => render(elem, scope.visopts, scope.music);
+      scope.$watch('music', refresh);
+      scope.$watch('visopts', refresh);
     },
   };
 });
